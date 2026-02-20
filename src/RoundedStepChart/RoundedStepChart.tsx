1a8650cee9ac8d3b3ad2f29d552b93b95abf492b
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2026-02-20 22:41:58 +0100

    Update draw logic

--- src/RoundedStepChart/RoundedStepChart.tsx
+++ src/RoundedStepChart/RoundedStepChart.tsx
@@ -59,48 +59,83 @@ export function RoundedStepChart({ data }: { data: GraphData }) {
     let xspace = (size?.width || 100) / dates.length
     let yspace = size?.height || 100
 
+    let createPoint = (v: number, max: number, i: number, serie: GraphSerie, yunit: number, color: string, date: String) => {
+        let el = <circle cx={i*xspace} cy={(max-v)*yunit} r="6" stroke={color} fill="transparent" strokeWidth="2">
+            <title>(id {serie.id}) {serie.label} ({serie.dataToString(v)}, {dateToHuman(date as string)})</title>
+        </circle>
+        return el
+    }
+
     let colors = getPalette(data.series.length)
     data.series.forEach((serie: GraphSerie) => {
         let color = colors[serie.id % colors.length]
-        let wasShowingRightCircle = true
-        for (let i = 0; i < serie.points.length - 1; i++) {
-            let date0 = dates[i]
-            let date1 = dates[i+1]
-            let v0 = serie.points[i]
-            let v1 = serie.points[i+1]
-            let max0 = (data.normalize ? date0.max : all_max)*1.01
-            let max1 = (data.normalize ? date1.max : all_max)*1.01
-            let yunit0 = yspace / max0
-            let yunit1 = yspace / max1
-            let showRightCircle = v1 && v1 !== v0 || i === serie.points.length - 2
-            if (v0 && v1) {
-                let x1 = (i+0.5)*xspace + (wasShowingRightCircle ? 6 : 0)
-                let y1 = (max0-v0)*yunit0
-                let c1x = (i+1)*xspace
-                let c1y = (max0-v0)*yunit0
-                let c2x = (i+1)*xspace
-                let c2y = (max1-v1)*yunit1
-                let x2 = (i+1.5)*xspace - (showRightCircle ? 6 : 0)
-                let y2 = (max1-v1)*yunit1
-                let el = <path d={`M ${x1} ${y1} C ${c1x} ${c1y}, ${c2x} ${c2y}, ${x2} ${y2}`} stroke={color} fill="transparent" strokeWidth="2" />
-                flows.push(el)
-            } /* else if (v0) {
-                let el = <circle cx={i*xspace} cy={(max0-v0)*yunit0} r="6" stroke={color} fill="transparent" strokeWidth="2"><title>Test</title></circle>
-                flows.push(el)
-            } */
-           if (showRightCircle) {
-                let el = <circle cx={(i+1.5)*xspace} cy={(max1-v1)*yunit1} r="6" stroke={color} fill="transparent" strokeWidth="2">
-                    <title>(id {serie.id}) {serie.label} ({serie.dataToString(v1)}, {dateToHuman(date1.date as string)})</title>
-                </circle>
-                flows.push(el)
+        // let wasShowingRightCircle = true
+
+        let previous_value: number | null = null
+        let value = serie.points[0]
+        let last_drawn = -1
+
+        for (let i = 0; i < serie.points.length; i++) {
+
+            let next_value = serie.points[i+1]
+
+            if (!value) {
+                value = next_value
+                last_drawn = -1
+                continue
             }
-            if (v0 && (i === 0 || !v1)) {
-                let el = <circle cx={(i+0.5)*xspace} cy={(max0-v0)*yunit0} r="6" stroke={color} fill="transparent" strokeWidth="2">
-                    <title>(id {serie.id}) {serie.label} ({serie.dataToString(v0)}, {dateToHuman(date0.date as string)})</title>
-                </circle>
-                flows.push(el)
+            if (value === previous_value && value === next_value) {
+                value = next_value
+                continue
             }
-            wasShowingRightCircle = showRightCircle
+
+            if (last_drawn > -1) {
+                if (last_drawn < i-1) {
+                    let max0 = (data.normalize ? dates[i].max : all_max)*1.01
+                    let yunit0 = yspace / max0
+
+                    let max1 = (data.normalize ? dates[i+1].max : all_max)*1.01
+                    let yunit1 = yspace / max1
+
+                    let x1 = (last_drawn+0.5)*xspace + 6
+                    let y1 = (max0-previous_value!)*yunit0
+                    let x2 = (i+0.5)*xspace - 6
+                    let y2 = (max1-value)*yunit1
+                    let el = <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} fill="transparent" strokeWidth="2"></line>
+                    flows.push(el)
+                } else if (previous_value && value) {
+                    let max0 = (data.normalize ? dates[i].max : all_max)*1.01
+                    let yunit0 = yspace / max0
+
+                    let max1 = (data.normalize ? dates[i+1].max : all_max)*1.01
+                    let yunit1 = yspace / max1
+
+                    let x1 = (i-0.5)*xspace + 6
+                    let y1 = (max0-previous_value!)*yunit0
+                    let c1x = (i)*xspace
+                    let c1y = (max0-previous_value!)*yunit0
+                    let c2x = (i)*xspace
+                    let c2y = (max1-value)*yunit1
+                    let x2 = (i+0.5)*xspace - 6
+                    let y2 = (max1-value)*yunit1
+                    let el = <path d={`M ${x1} ${y1} C ${c1x} ${c1y}, ${c2x} ${c2y}, ${x2} ${y2}`} stroke={color} fill="transparent" strokeWidth="2" />
+                    flows.push(el)
+                }
+            }
+
+            if (value && value !== previous_value || next_value !== value) {
+                let date = dates[i]
+                let max = (data.normalize ? date.max : all_max)*1.01
+                let yunit = yspace / max
+
+                let el = createPoint(value, max, i+0.5, serie, yunit, color, date.date)
+                flows.push(el)
+                last_drawn = i
+            }
+
+            previous_value = value
+            value = next_value
+
         }
     })
     // console.log('size', size)
